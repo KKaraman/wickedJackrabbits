243c56490dafec1ebdf813a3c31072386b222be3
Richard <richardbrianhall@gmail.com>
2020-12-22 17:14:38 -0600

    login pages

--- src/pages/pagetwo.jsx
+++ src/pages/pagetwo.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import Project from "../components/Project";
 import Wrapper from "../components/Wrapper";
 import Title from "../components/Title";
 import projects from "../projects.json";
@@ -14,19 +13,16 @@ class PageTwo extends Component {
   // Map over this.state.projects and render a project component for each one
   render() {
     return (
-      <Wrapper>
-        <Title>Agent Name</Title>
-        <h6>Add Houses Here</h6>
-        {this.state.projects.map(project => (
-          <Project
-            id={project.id}
-            key={project.id}
-            name={project.name}
-            screenshot={project.screenshot}
-            deployment={project.deployment}
-            repository={project.repository}
-          />
-        ))}
+      <Wrapper> 
+        <Title>Agent Login</Title>
+        <div class="Container">
+        <textarea className="agentEmail" placeholder="email"></textarea>
+        <textarea className="agentPassword" placeholder="password"></textarea>
+        <div class="center">
+        <button className="agentLogin" onclick="agentLogin()">Log In</button>
+        </div>
+        </div>
+        <p></p>
       </Wrapper>
     );
   }
