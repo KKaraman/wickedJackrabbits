fdfed98f0b344a068de12be9b1790ada1e1df708
Richard <richardbrianhall@gmail.com>
2021-01-15 10:48:00 -0600

    homeowner and agent login, need to edit homeowner consts

--- client/src/pages/sellerlogin.jsx
+++ client/src/pages/sellerlogin.jsx
@@ -1,29 +1,50 @@
-import React, { Component } from "react";
-import "./login.css";
+import React, { useState } from "react";
+import { Row, Col, Button, Container } from "react-bootstrap";
+import axios from "axios";
+import "./signup.css";
 
-class PageThree extends Component {
-  // Setting this.state.friends to the friends json array
-  state = {
-    projects
-  };
+const AgentLogin = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-
-  // Map over this.state.projects and render a project component for each one
-  render() {
-    return (
-      <div>
-       <h1> Seller Login</h1>
-        <div class="Container">
-        <textarea className="sellerEmail" placeholder="email"></textarea>
-        <textarea className="sellerPassword" placeholder="password"></textarea>
-        <div class="center">
-        <Button className="sellerLogin" href="/homeownermain"> Log In</Button>
-        </div>
-        </div>
-        </div>
-      
-    );
+  const submitAgent = () => {
+    axios.post("/api/login", {
+      email,
+      password
+    }).then((res) => {
+      console.log(res)
+    })
   }
+  // Map over this.state.projects and render a project component for each one
+  return (
+    <Container>
+      <div className="signupWrap">
+      <Row>
+    <div className="signupInnerWrap">
+      <Col>
+    <h1>Homeowner Login</h1>
+
+      <input onChange={(e) => { setEmail(e.target.value) }} className="txtarea" type="text" placeholder="Email*"></input>
+      <p></p>
+      <input onChange={(e) => { setPassword(e.target.value) }} className="txtarea" type="text" placeholder="Password*"></input>
+
+      <p></p>
+        <Button onClick={submitAgent} className="agentLogin btn" >Log In</Button>
+        <p></p>
+        <h5>Not the Homeowner?</h5>                         
+        
+        <Button  href="/" id="login" type="submit">Sign up </Button> 
+        <Button  href="/agentlogin" id="login" type="submit">Log In as an agent</Button>
+
+ 
+
+    </Col>
+    </div>
+    </Row>
+    </div>
+    </Container>
+    );
+
 }
 
-export default PageThree;
+export default AgentLogin;
