9f892a0af9277ebcc36ef7b2f63e56cd0f281feb
Richard <richardbrianhall@gmail.com>
2021-01-15 12:39:53 -0600

    createoffer page formatted

--- client/src/pages/offer/createOffer.jsx
+++ client/src/pages/offer/createOffer.jsx
@@ -2,7 +2,7 @@ import { Row, Col, Button, Container } from "react-bootstrap";
 import React, {useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
-import "./offer.css";
+import "../signup.css";
 
 
 const createOffer = () => {
@@ -25,26 +25,24 @@ const createOffer = () => {
     return (
        
             <Container>
-                <div className="offerWrap">
+            <div className="signupWrap">
                     <Row>
-                   
-                    <input onChange={(e) => {setDateOffered(e.target.value)}} className="txtarea" type="text" placeholder="Date Offered"/>
-                    <p></p>
-                    <Col className="offerInnerWrap">
-                        Buyer's Name
-                    
-                    <input onChange={(e) => {setName(e.target.value)}} className="txtarea" type="text" placeholder="name"/>
+                    <div className="signupInnerWrap">
+                        <Col>
+                        <Col>
+                        <h1>Create Offer</h1>
+                    <input onChange={(e) => {setDateOffered(e.target.value)}} className="txtarea" type="text" placeholder="Date of Offer"/>
+                        
+                    <input onChange={(e) => {setName(e.target.value)}} className="txtarea" type="text" placeholder="Buyer's Name"/>
+
+                    <input onChange={(e) => {setAmount(e.target.value)}} className="txtarea" type="text" placeholder="Offer Amount"/>
                     <p></p>
 
-                    Offer
-                    <input onChange={(e) => {setAmount(e.target.value)}} className="txtarea" type="text" placeholder="$"/>
-                    <p></p>
-
-                    Earliest Closing Date
+                    <h5>Earliest Closing Date</h5>
                     <input onChange={(e) => {setClosingDate(e.target.value)}} className="txtarea" type="date" placeholder="Closing Date"/>
                         <p></p>   
                         
-                    <p className="payType">Payment Type 
+                    <p className="payType"><h5>Payment Type</h5> 
                     <select onChange={(e) => {setMoneyType(e.target.value)}} className="txtarea" type="offer" placeholder="Money Type">
                             <option className="txtarea" value="cash">Cash</option>
                             <option className="txtarea" value="loan">Bank Loan</option>
@@ -56,8 +54,8 @@ const createOffer = () => {
 
 
                    </Col>
-                
-                    
+                   </Col>
+                   </div>
                     </Row>
                 </div>
             </Container>
