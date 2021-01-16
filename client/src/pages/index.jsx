f126de9b3c682f5f1c11e2dbecbaf9ca0ce0846c
Richard <richardbrianhall@gmail.com>
2021-01-16 08:41:39 -0600

    generated homes css

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Image, Button, Card } from "react-bootstrap";
 import {NavLink} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../App.css";
+import ".././App.css";
 import Placeholder from "../images/placeholder.png";
 import axios from "axios";
 
@@ -44,6 +44,7 @@ const MainPage = () => {
           <Button type="button" className="btn addHome btn-primary" href="/addHome">Add home</Button>
           </div>
         </Row>
+        <p></p>
 
         {homeData.Users && homeData.Users.length !== 0 && homeData.Users.map(item => {
 
@@ -55,41 +56,70 @@ const MainPage = () => {
                
                 <Col>
                   <NavLink to={`/createoffer/`}>
+                    <div className="card">
                     <Card>
+                      <div className="genImage">
                       <Image className="card-img-top" src={Placeholder} fluid />
                       {/* implement agent updated photos */}
-                    
-                      <Col>
-                        <div >
+                      </div>
+                      <div className="genWrap">
+
+
+                          <div className="genPart">
+                            <p>
+                              <div className="innerPart">
+                                 Address 
+                              </div>
+                              <p></p>
+                              <div className="innerGenPart">
+                                  <p>{home.address}</p>
+                              </div>
+                            </p>
+                          </div>
+
+                          <div className="genPart">
                           <p>
-                            Address: {home.address}
+                                  <div className="innerPart">
+                            Listing Price
+                            </div>
+                             <p></p> 
+                             <div className="innerGenPart">
+                              <p>${home.listingPrice}</p>
+                              </div>
                           </p>
-                        </div>
-                      Listing Price : ${home.listingPrice}
+                              </div>
 
-                      </Col> 
 
-                      <Col>
-                        <Row>
-                          
-        
-                          <Col>
-                            <div>
-                              <p>
-                                Offers: 
+                            <div className="genPart">
+                            <p>
+                                  <div className="innerPart">
+                                Offers 
+                                </div>
+                                <p></p>
+                                <div className="innerGenPart">
+                                  <p>Generated Text</p>
+                                  </div>
                               </p>
                             </div>
-                            <Col>
-                              <div>
-                                <p>
-                                  Sellers name:  {item.firstName} {item.LastName}
-                                </p>
+
+                              <div className="genPart">
+                                  <p>
+                                  <div className="innerPart">
+                                  Homeowner 
+                                  </div> 
+                                  </p>
+                                  <p></p>
+                                  <div className="innerGenPart">
+                                    <p>{item.firstName} {item.LastName}
+                                  </p>
+                                  </div>
                               </div>
-                            </Col>
-                          </Col>
-                        </Row>
-                      </Col>
+
+                            </div>
+
+
                     </Card>
+                    </div>
                     </NavLink>
                   </Col>
                 
