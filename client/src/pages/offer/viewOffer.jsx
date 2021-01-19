431520080c71438deab77aff9813704ba2c89207
Richard <richardbrianhall@gmail.com>
2021-01-18 18:29:46 -0600

    view offers css

--- client/src/pages/offer/viewOffer.jsx
+++ client/src/pages/offer/viewOffer.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import {useParams} from "react-router";
 import {Link} from "react-router-dom"
 import axios from "axios";
+import "./offer.css";
 
 const viewOffer = () => {
     const [agentData, setAgentData] = useState({})
@@ -23,18 +24,57 @@ const viewOffer = () => {
             <div>
                 <h3>Offers</h3>
             </div>
+            <div className="viewOffers">
+
+
+            <Row>
+                        <div className="offerBitsWrap">
+                        <Link href="/selectedoffer/:id">
+                            <div className="offerBits">
+                            <Col>Buyer</Col></div>
+                        </Link>
+                        <div className="offerBits">
+                        <Col>
+                        Date of Offer</Col>
+                        </div>
+                        <div className="offerBits">
+                        <Col>
+                        Offer Amount</Col>
+                        </div>
+                        <div className="offerBits">
+                        <Col>
+                        Earliest Closing Date</Col>
+                        </div>
+                        </div>
+                    </Row>
+
             {agentData.Offers && agentData.Offers.length !== 0 && agentData.Offers.map(item => {
                 return (
+
                     <Row>
+                        <div className="offerBitsWrap">
                         <Link href="/selectedoffer/:id">
-                            <Col>{item.id}. {item.name}</Col>
+                            <div className="offerBits">
+                            <Col>{item.id}. {item.name}</Col></div>
                         </Link>
-                        <Col>{item.id}. {item.dateOffered}</Col>
-                        <Col>{item.id}. {item.amount}</Col>
-                        <Col>{item.id}. {item.closingDate}</Col>
+                        <div className="offerBits">
+                        <Col>
+                        {item.id}. {item.dateOffered}</Col>
+                        </div>
+                        <div className="offerBits">
+                        <Col>
+                        {item.id}. {item.amount}</Col>
+                        </div>
+                        <div className="offerBits">
+                        <Col>
+                        {item.id}. {item.closingDate}</Col>
+                        </div>
+                        </div>
                     </Row>
+                    
                 )
             })}
+            </div>
         </Container>
     )
 }
